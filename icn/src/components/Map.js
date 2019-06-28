b51732cf7b02c74687923522b08b74790bc6daa2
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-06-28 10:10:41 -0400

    Add ability to click and add countries from map

--- icn/src/components/Map.js
+++ icn/src/components/Map.js
@@ -3,9 +3,16 @@ import getCountryCode from "../mock_data/ISO"
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_worldHigh from "@amcharts/amcharts4-geodata/worldHigh"
-
+import {Modal, Form, Input} from 'antd';
+import {addData} from '../actions'
+import {connect} from 'react-redux'
 
 class Map extends React.Component {
+    state = {
+        visible: false,
+        activeCountry: ''
+    }
+
     componentDidMount() {
         this.displayMap()
     }
@@ -65,12 +72,48 @@ class Map extends React.Component {
         let desiredCountry = this.props.countries.filter(country => country.country === countryClicked)
         if(desiredCountry.length)
             this.props.history.push(`/dashboard/countries/community/${desiredCountry[0].id}`)
+        else
+            this.setState({...this.state, activeCountry: countryClicked}, () => this.showModal())
+
+    }
+    showModal = () => {
+        this.setState({
+            visible: true,
+        })
+    }
+    handleOk = e => {
+        e.preventDefault();
+        this.props.addData(`/api/country/`, {country: this.state.activeCountry}, 'country')
+        this.setState({
+            visible: false,
+            activeCountry: ''
+        })
+    }
+    handleCancel = e => {
+        this.setState({
+            visible: false,
+            activeCountry: ''
+        })
     }
     render(){
         return (
+            <>
             <div id="chartdiv" style={{width:'100%', height: '700px'}}></div>
+            <Modal
+                title={`Add country`}
+                visible={this.state.visible}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+            >
+                <Form onSubmit={this.handleOk}>
+                    <Form.Item label={`Country name:`}>
+                        <Input required={true} value={this.state.activeCountry} onChange={this.handleChange}/>
+                    </Form.Item>
+                </Form>
+            </Modal>
+            </>
         )
     }
 }
 
-export default Map
+export default connect(null, {addData})(Map)
